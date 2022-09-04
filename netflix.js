97327d761184c6305c9417b61c2542b0ac54ddbe
joao-ig-silva <joaoitalogsilva@gmail.com>
2022-09-04 14:48:58 -0300

    Netflix Uninassau
    Trabalho da cadeira Tópicos Integradores
    Professora: Flávia Agostinho

---
+++ netflix.js
@@ -0,0 +1,9 @@
+var listaFilmes = [
+    "https://ingresso-a.akamaihd.net/img/cinema/cartaz/7766-cartaz.jpg",
+    "https://s2.glbimg.com/zyaaSEbf4qZ6BNkc778luqyVhkE=/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2019/r/0/u4CVkTSpKCAj94NO5G1w/05cul-200-dvd2-d30-img01.jpg",
+    "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/92/91/32/20224832.jpg"
+  ];
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
